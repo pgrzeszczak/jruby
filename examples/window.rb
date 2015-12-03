95c5e3c245660b5928a531f1bd005c936d38f268
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2015-12-03 22:54:23 +0100

    Small things

--- examples/window.rb
+++ examples/window.rb
@@ -2,6 +2,7 @@ require 'java'
 
 JFrame = javax.swing.JFrame
 JLabel = javax.swing.JLabel
+
 frame  = JFrame.new "Hello!"
 frame.setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
 frame.setSize 600, 400
@@ -9,4 +10,4 @@ frame.setSize 600, 400
 label = JLabel.new "Is it me you're looking for?"
 frame.add label
 
-frame.setVisible(true)
+frame.setVisible true
