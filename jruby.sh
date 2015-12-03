30f7cb62dd74a30f3740379ecfd8207c8ec7c939
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2015-12-03 18:47:16 +0100

    Mount X sockets as readonly

--- jruby.sh
+++ jruby.sh
@@ -2,7 +2,7 @@
 
 docker run --rm -ti \
     -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     -v $(pwd):/project \
     -w /project \
     pgrzeszczak/jruby \
